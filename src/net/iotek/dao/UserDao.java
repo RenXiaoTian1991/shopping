830b5184a4a2537699e7a2536ce0a1661a3c6f7e
iotek-baipeng <p.bai@iotek.com.cn>
2017-01-20 16:12:03 +0800

    修改
    初始化项目

--- src/net/iotek/dao/UserDao.java
+++ src/net/iotek/dao/UserDao.java
@@ -3,6 +3,7 @@ package net.iotek.dao;
 import net.iotek.entity.User;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
 import javax.annotation.Resource;
 
 /**
@@ -13,23 +14,27 @@ import javax.annotation.Resource;
 @Repository
 public class UserDao {
 
-    @Resource
-    private SqlSessionTemplate sqlSessionTemplate;
+	@Resource
+	private SqlSessionTemplate sqlSessionTemplate;
 
-    public User selectUser(User user){
-        return sqlSessionTemplate.selectOne("User.selectUser",user);
-    }
+	public User selectUser(User user) {
 
-    public void updatePassword(User user){
-        sqlSessionTemplate.update("User.updatePassword",user);
-    }
+		return sqlSessionTemplate.selectOne("User.selectUser", user);
+	}
 
-    public void addUser(User user){
-        sqlSessionTemplate.insert("User.addUser",user);
-    }
+	public void updatePassword(User user) {
 
-    public void updateUser(User user){
-        sqlSessionTemplate.update("User.updateUser",user);
-    }
+		sqlSessionTemplate.update("User.updatePassword", user);
+	}
+
+	public void addUser(User user) {
+
+		sqlSessionTemplate.insert("User.addUser", user);
+	}
+
+	public void updateUser(User user) {
+
+		sqlSessionTemplate.update("User.updateUser", user);
+	}
 
 }
